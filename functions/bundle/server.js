b4083dd06d4ceb22ea23a2cda470cd134747080a
Sehajpreet Singh <sehajmagan@gmail.com>
2023-07-15 19:35:19 -0400

    updated server

---
+++ functions/bundle/server.js
@@ -0,0 +1,53 @@
+const ApolloServer = require("apollo-server").ApolloServer;
+const ApolloServerLambda = require("apollo-server-lambda").ApolloServer;
+const { gql } = require("apollo-server-lambda");
+const axios = require("axios");
+
+const typeDefs = gql`
+  type User {
+    id: ID
+    login: String
+    avatar_url: String
+  }
+
+  type Query {
+    users: [User]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    users: async () => {
+      try {
+        const users = await axios.get("https://api.github.com/users");
+        return users.data.map(({ id, login, avatar_url }) => ({
+          id,
+          login,
+          avatar_url,
+        }));
+      } catch (error) {
+        throw error;
+      }
+    },
+  },
+};
+
+function createLambdaServer() {
+  return new ApolloServerLambda({
+    typeDefs,
+    resolvers,
+    introspection: true,
+    playground: true,
+  });
+}
+
+function createLocalServer() {
+  return new ApolloServer({
+    typeDefs,
+    resolvers,
+    introspection: true,
+    playground: true,
+  });
+}
+
+module.exports = { createLambdaServer, createLocalServer };
